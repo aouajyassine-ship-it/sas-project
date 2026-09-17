61c0912bd5dff7c5b1515a37b71d2f3aff2370cc
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-17 10:59:48 +0100

    progression

--- progression.js
+++ progression.js
@@ -1,4 +1,13 @@
+
 function normaliserNom(nom) {
-    
-    
-}
+    nom = nom.trim();
+    let mots = nom.split(" ");
+    let result = "";
+    for ( let i = 0 ; i < mots.length ; i++) {
+        let mot = mots[i];
+        let premiereLettre = mot[0].toUpperCase();
+        let reste = mot.slice(1).toLowerCase();
+        result = result + premiereLettre + reste + " ";
+    }
+    return result;
+} 
